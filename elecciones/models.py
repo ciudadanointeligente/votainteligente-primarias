5f629e29792e3c3a6907ea57c24d7a86c7199cfb
Marcel Augsburger <maugsburger@votainteligente.cl>
2013-06-06 18:43:04 -0400

    Modifiying email sending a test is failing

--- elecciones/models.py
+++ elecciones/models.py
@@ -237,7 +237,8 @@ class Pregunta(models.Model):
 			mensaje = texto_introduccion + u'\r\rYo, ' + self.remitente + ' quiero saber: \r\r' + self.texto_pregunta + texto_cierre
 			destinaciones = Contacto.objects.filter(candidato=candidato)
 			for destinacion in destinaciones:
-				store_mail(subject, mensaje, settings.DEFAULT_FROM_EMAIL,[destinacion.valor])
+				pass
+				#store_mail(subject, mensaje, settings.DEFAULT_FROM_EMAIL,[destinacion.valor])
 
 			
 
