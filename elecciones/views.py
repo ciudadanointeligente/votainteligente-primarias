fddbada50983594c45600075c2f44f48a69d0ac2
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-06-25 13:24:56 -0400

    Adding debug tool and also improving preguntales queries

--- elecciones/views.py
+++ elecciones/views.py
@@ -99,23 +99,7 @@ class EleccionPreguntales(CreateView):
         contactos_candidato = Contacto.objects.filter(candidato__in = candidatos_eleccion)
         candidatos = candidatos_eleccion.filter(contacto__in = contactos_candidato)
         preguntas = Pregunta.objects.filter(candidato__in = candidatos_eleccion).filter(aprobada = True).distinct()
-        conversaciones = {}
-        for pregunta in preguntas:
-            texto_pregunta = pregunta.texto_pregunta
-            mensaje = {}
-            respuestas = {}
-            respuestas_pregunta = Respuesta.objects.filter(pregunta=pregunta)
-            for respuesta_pregunta in respuestas_pregunta:
-                nombre_candidato = respuesta_pregunta.candidato.nombre
-                respuestas[nombre_candidato] = respuesta_pregunta
-            nombre_emisor = pregunta.remitente
-            mensaje[texto_pregunta] = respuestas
-            conversaciones[nombre_emisor] = mensaje
-
-
-
         context['preguntas'] = preguntas
-        context['conversaciones'] = conversaciones
         context['candidatos'] = candidatos
         todas_las_elecciones = Eleccion.objects.all()
         context['elecciones'] = todas_las_elecciones
